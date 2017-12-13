3964515325a2240ba053ba6fe76330376e659273
pchiu <paula.chiu@sfgov.org>
2017-12-12 17:06:21 -0800

    Dash examples

---
+++ plotly/app_examples/app_graph_click.py
@@ -0,0 +1,68 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import json
+from textwrap import dedent as d
+
+
+app = dash.Dash(__name__)
+
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='basic-interactions',
+        figure={
+            'data': [
+                {
+                    'x': [1, 2, 3, 3.5, 4],
+                    'y': [4, 1, 3, 3.5, 5],
+                    'text': ['a', 'b', 'c', 'd'],
+                    'customdata': ['c.a', 'c.b', 'c.c', 'c.d'],
+                    'name': 'Trace 1',
+                    'mode': 'markers',
+                    'marker': {'size': 12}
+                },
+                {
+                    'x': [1, 2, 2.5, 3, 4],
+                    'y': [9, 4, 2.5, 1, 4],
+                    'text': ['w', 'x', 'y', 'z'],
+                    'customdata': ['c.w', 'c.x', 'c.y', 'c.z'],
+                    'name': 'Trace 2',
+                    'mode': 'markers',
+                    'marker': {'size': 12}
+                }
+            ]
+        }
+    ),
+
+    html.Div(className="row", children=[
+        html.Div([
+            dcc.Markdown(d("""
+                **Click Data**
+
+                Click on points in the graph.
+            """)),
+            html.Pre(id='click-data', style=styles['pre']),
+        ], className="three columns"),
+    ])
+])
+
+
+
+@app.callback(
+    Output('click-data', 'children'),
+    [Input('basic-interactions', 'clickData')])
+def display_click_data(clickData):
+    return json.dumps(clickData, indent=2)
+    # return "Hello"
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
